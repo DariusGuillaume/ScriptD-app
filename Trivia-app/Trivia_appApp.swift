8b95bc1c45161aa6dc2f24b0474b3f922c2a5310
DariusGuillaume <dariusguillaume2@gmail.com>
2024-02-08 04:25:25 -0500

    Initial Commit

---
+++ Trivia-app/Trivia_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia_appApp.swift
+//  Trivia-app
+//
+//  Created by Darius Guillaume on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Trivia_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
