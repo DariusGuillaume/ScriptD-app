c276426cbf790eb46d37b3ab2705e62608397e51
DariusGuillaume <dariusguillaume2@gmail.com>
2024-02-09 02:24:06 -0500

    Button functionality added

---
+++ Trivia-app/Views/TriviaView.swift
@@ -0,0 +1,18 @@
+//
+//  TriviaView.swift
+//  Trivia-app
+//
+//  Created by Darius Guillaume on 2/9/24.
+//
+
+import SwiftUI
+
+struct TriviaView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TriviaView()
+}
